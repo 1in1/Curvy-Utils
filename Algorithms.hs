f51266836c4b3c5c975215be95fbbbf758f09316
l <info@datas.ink>
2022-10-04 10:58:34 +0100

    up

--- Algorithms.hs
+++ Algorithms.hs
@@ -16,11 +16,11 @@ import EllipticCurves
 -- Algorithm of Velu - c.f. Velu, Isogenies between Elliptic Curves
 quotientCurve :: (Eq k, Fractional k) => Curve k -> [ProjectivePoint k] -> Curve k
 quotientCurve (Curve a1 a3 a2 a4 a6) f = quot where
-    f2 = filter ((== Infinity) . ellipticNMult (Curve a1 a3 a2 a4 a6) 2) f
+    f2 = filter (/= Infinity) $ filter ((== Infinity) . ellipticNMult (Curve a1 a3 a2 a4 a6) 2) f
     r [] r' = r'
     r (x:xs) r' | (x `elem` r') || (ellipticInverse (Curve a1 a3 a2 a4 a6) x `elem` r') = r xs r'
                 | otherwise = r xs (x:r') 
-    s = f2 ++ r ((f \\ [Infinity]) \\ f2) []
+    s = f2 ++ r (f \\ (Infinity:f2)) []
 
     gxq (Planar xq yq) = 3*xq^2 + 2*a2*xq + a4 - a1*yq
     gyq (Planar xq yq) = -2*yq - a1*xq - a3
@@ -69,14 +69,21 @@ curvePointsOnFp curve = Infinity:filter (curveContainsPoint curve) (Planar <$> l
     p = natVal (Proxy :: Proxy p)
     lst = map fromInteger [0..(p-1)]
 
-isSingular :: (Eq k, Num k) => Curve k -> ProjectivePoint k -> Bool
-isSingular curve = (== (0,0,0)) . formalDerivatives curve 
-
+-- Returns the formal derivative function in x, y, z coordinates
 formalDerivatives :: (Num k) => Curve k -> ProjectivePoint k -> (k, k, k)
 formalDerivatives (Curve a1 a3 a2 a4 a6) = ((,,) <$> dxF <*> dyF <*> dzF) . homogenizeCoords where
     dxF (x, y, z) = a1*y*z - 3*x^2 - 2*a2*x*z - a4*z^2
     dyF (x, y, z) = 2*y*z + a1*x*z + 2*a3*z^2
     dzF (x, y, z) = y^2 + a1*x*y + 2*a3*y*z - a2*x^2 - 2*a4*x*z - 3*a6*z^2
+
+-- Test if a point is singular. Note that by definition an elliptic curve is non-singular, but 
+-- it is of interest to know when and where its reduction modulo some prime is singular
+isSingular :: (Eq k, Num k) => Curve k -> ProjectivePoint k -> Bool
+isSingular curve = (== (0,0,0)) . formalDerivatives curve 
+
+-- Identify singular points on the curve
+singularities :: forall p . KnownNat p => Curve (PrimeFieldElem p) -> [ProjectivePoint (PrimeFieldElem p)]
+singularities = filter <$> isSingular <*> curvePointsOnFp
 
 -- Assume working over a field of characteristic /= 2, 3
 -- Assume we have a cubic of the form s1u^3 + s2u^2v + s3uv^2 + s4v^3 + s5u^2 + s6uv + s7v^2 + s8u + s9v = 0
@@ -98,7 +105,7 @@ nagellsAlgorithm (GeneralCubic s1 s2 s3 s4 s5 s6 s7 s8 s9) = curve where
     -- The algebra is unpleasant here - what we can do, however, is interpolation to get the coefficients
     -- once we have the function itself
     r | e3 /= 0 = e2/e3
-      | otherwise = 1 -- Have to handle the fact that W' becomes U'!!!
+      | otherwise = 1 
     f3' u' v' 
         | e3 /= 0 = s1*u'^3 + s2*(u'^2)*v' + s3*u'*v'^2 + s4*v'^3
         | otherwise = 
