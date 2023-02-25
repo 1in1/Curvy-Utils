1c2ea6b70d18c8ecf5b7c93817310f359aa68adb
1in1 <info@datas.ink>
2023-02-25 05:22:18 +0000

    add point counting over arbitrary finite fields

--- src/Data/EllipticCurves/Algorithms/PrimeFieldCurves.hs
+++ src/Data/EllipticCurves/Algorithms/PrimeFieldCurves.hs
@@ -1,5 +1,6 @@
 module Data.EllipticCurves.Algorithms.PrimeFieldCurves (
       curvePointsOnFp
+    , countCurvePointsOverExtension
     , singularities
     ) where
 
@@ -7,7 +8,6 @@ import Data.Proxy
 import Data.Ratio
 import GHC.TypeLits
 import Numeric.AD
-import Numeric.AD.Internal.Forward
 import Numeric.AD.Internal.Tower
 
 import Data.EllipticCurves
@@ -19,6 +19,40 @@ curvePointsOnFp :: forall p . KnownNat p => Curve (PrimeFieldElem p) -> [Project
 curvePointsOnFp curve = Infinity:filter (curveContainsPoint curve) (Planar <$> lst <*> lst) where
     p = natVal (Proxy :: Proxy p)
     lst = map fromInteger [0..(p-1)]
+
+-- Find the order of E(F_p^r), for arbitrary r, using the zeta function
+countCurvePointsOverExtension :: forall p . KnownNat p => Curve (PrimeFieldElem p) -> [Integer]
+countCurvePointsOverExtension curve = curvePointCounts where
+    q = toRational $ natVal (Proxy :: Proxy p) :: Rational
+    q' = auto q :: AD s (Numeric.AD.Internal.Tower.Tower Rational)
+    n = toRational $ length $ curvePointsOnFp curve :: Rational
+    a = q + 1 - n -- :: AD s (Numeric.AD.Internal.Tower.Tower Rational)
+    a' = auto a :: AD s (Numeric.AD.Internal.Tower.Tower Rational)
+
+    -- Note this is exp(f(t)), for f(t) = n1 t + n2/2 t^2 + n3/3 t^3 + ...
+    zeta :: AD s (Numeric.AD.Internal.Tower.Tower Rational) -> AD s (Numeric.AD.Internal.Tower.Tower Rational)
+    zeta t = (1 - a'*t + q'*t^i2) / ((1 - t) *(1 - q'*t))
+
+    -- The derivatives of the zeta function will be:
+    -- d^k/dt^k (exp(f(t))) = exp(f(t)) ( \sum_{i1+...+ir=k, ij>0} d^i1/dt^i1 f(t) * ... * d^ir/dt^ir f(t) )
+    -- But we note that d^r/dt^r f(t) | t = 0 is #E(F_p^r)
+    derivatives = diffs zeta 0 :: [Rational]
+    idxs :: Int -> [[Int]]
+    idxs = (map idxs' [0..] !!) where
+        idxs' 0 = [[]]
+        idxs' m = [ i:xs | i <- [1..m], xs <- idxs (m-i) ] -- TODO can throw away ordering 
+
+    fDerivs :: Int -> Rational
+    fDerivs = (map fDerivs' [0..] !!) where
+        fDerivs' 0 = 0
+        fDerivs' 1 = derivatives!!1
+        fDerivs' i = (derivatives!!i) - sum (init $ map (product . map fDerivs) $ idxs i)
+
+    -- We need to account for the extra factors gained by differentiating multiple times
+    factorials :: [Rational]
+    factorials = scanl1 (*) [1..]
+    curvePointCounts = map numerator $ zipWith (/) (map fDerivs [1..]) (1:factorials)
+
 
 -- Identify singular points on the curve
 singularities :: forall p . KnownNat p => Curve (PrimeFieldElem p) -> [ProjectivePoint (PrimeFieldElem p)]
--- test/Data/EllipticCurves/Algorithms/PrimeFieldCurvesSpec.hs
+++ test/Data/EllipticCurves/Algorithms/PrimeFieldCurvesSpec.hs
@@ -1,4 +1,4 @@
-module Data.EllipticCurves.Algorithms.PrimeFieldCurvesSpec where
+module Data.EllipticCurves.Algorithms.PrimeFieldCurvesSpec (spec) where
 
 import Test.Hspec
 import Test.QuickCheck
@@ -11,18 +11,26 @@ spec :: Spec
 spec = do
     describe "Algorithms.PrimeFieldCurves.curvePointsOnFp" $ do
         it "correctly discovers all points on the finite field" $ do
-            curvePointsOnFp (Curve 1 2 3 4 5 :: Curve (PrimeFieldElem 5)) `shouldMatchList` [
-                Infinity, 
-                Planar (PrimeFieldElem 0) (PrimeFieldElem 0), 
-                Planar (PrimeFieldElem 0) (PrimeFieldElem 3), 
-                Planar (PrimeFieldElem 1) (PrimeFieldElem 3), 
-                Planar (PrimeFieldElem 1) (PrimeFieldElem 4), 
-                Planar (PrimeFieldElem 3) (PrimeFieldElem 1), 
-                Planar (PrimeFieldElem 3) (PrimeFieldElem 4)
+            curvePointsOnFp (Curve 1 2 3 4 5 :: Curve (PrimeFieldElem 5)) `shouldMatchList`
+                [ Infinity
+                , Planar (PrimeFieldElem 0) (PrimeFieldElem 0)
+                , Planar (PrimeFieldElem 0) (PrimeFieldElem 3)
+                , Planar (PrimeFieldElem 1) (PrimeFieldElem 3)
+                , Planar (PrimeFieldElem 1) (PrimeFieldElem 4)
+                , Planar (PrimeFieldElem 3) (PrimeFieldElem 1)
+                , Planar (PrimeFieldElem 3) (PrimeFieldElem 4)
+                ]
+
+    describe "Algorithms.PrimeFieldCurves.countCurvePointsOverExtension" $ do
+        it "correctly counts the numer of points in low degree" $ do
+            take 3 (countCurvePointsOverExtension (Curve 0 0 0 1 5 :: Curve (PrimeFieldElem 13))) `shouldMatchList`
+                [ 9
+                , 171
+                , 6075
                 ]
 
     describe "Algorithms.PrimeFieldCurves.singularities" $ do
         it "identifies the singularity on a curve y^2 = x^2(x - a) in FP_29" $ property
-            ((\ a -> singularities (Curve 0 0 (fromRational $ toRational a) 0 0 :: Curve (PrimeFieldElem 29)) == [
-                Planar (PrimeFieldElem 0) (PrimeFieldElem 0)
+            ((\ a -> singularities (Curve 0 0 (fromRational $ toRational a) 0 0 :: Curve (PrimeFieldElem 29)) ==
+                [ Planar (PrimeFieldElem 0) (PrimeFieldElem 0)
                 ]) :: Integer -> Bool)
